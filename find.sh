cbe7e0ac28360a969fd3fefe1e61f8a6e13d01df
jak0b <me@jak0b.com>
2025-06-29 18:47:27 +0200

    feat(config): remap PG UP/DOWN with HOME/END

--- find.sh
+++ find.sh
@@ -7,12 +7,11 @@ find_left() {
   strings "$1" | grep 's/bluetooth/controller/ll_sw/ull_central.c'
 }
 
-find_right() {
-  strings "$1" | grep 'WEST_TOPDIR/zephyr/subsys/bluetooth/controller/ll_sw/ull_peripheral.c'
-}
 
 find_left ~/Downloads/firmware/corneish_zen_v2_left-zmk.uf2
 find_right ~/Downloads/firmware/corneish_zen_v2_right-zmk.uf2
+
+
 
 
 find_right ~/Downloads/firmware/corneish_zen_v2_left-zmk.uf2
